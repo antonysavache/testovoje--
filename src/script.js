7d3fa0710e629e0fea0ffcbeddb8bf48782e87c1
antonysavache <89783847+antonysavache@users.noreply.github.com>
2021-11-23 18:05:03 +0200

    обработал 2 инпута по кнопке

--- src/script.js
+++ src/script.js
@@ -1,31 +1,66 @@
-console.log("hi");
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const ourInput = document.querySelector('[type="text"]');
-console.log(ourInput);
+const ourFirstInput = document.querySelector('.js-first');
+const ourActionInput = document.querySelector('.js-action');
+const mainBtn = document.querySelector('.js-btn')
+const actionsArray = ['съел', 'положил', 'взял'];
 
-let ourInputValue = null;
+// ourFirstInput.addEventListener('input', debounce(inputFirstValue, 300));
+// ourActionInput.addEventListener('input', debounce(inputActionValue, 300));
 
-ourInput.addEventListener("input", inputMosh);
+// function addToObj(){
+//   for (let i = 0; i < ourFirstInputValue.length; i += 1) {
+//     if (parseInt(ourFirstInputValue[i])) {
+//       arrWithAttributes.push(parseInt(ourFirstInputValue[i]));
+//       arrWithAttributes.push(ourFirstInputValue[i + 1]);
+//       firstObj[ourFirstInputValue[i + 1].slice(0, 4)] = ourFirstInputValue[i];
+//       console.log(firstObj)
+//       console.log(arrWithAttributes);
+//       ourFirstObj = firstObj;
+//     }
+//   }
+// }
 
-let ax = [];
+// эта функция смотрит что было на столе и записывает в массив отфиль-
+//трованное значение
 
-function inputMosh() {
-  const ourInputValue = ourInput.value.split(" ")
-  // console.log(ourInputValue);
+let ourFirstObj = null;
 
-  let arrayWithNumber = ourInputValue.map((element) => {
-    return parseInt(element);
-  });
-
+function inputFirstValue() {
+  inputActionValue()
+  const ourFirstInputValue = ourFirstInput.value.split(' ');
   let arrWithAttributes = [];
-
-for (let i = 0; i < ourInputValue.length; i+=1){
-    
-    if (parseInt(ourInputValue[i])){
-        arrWithAttributes.push(parseInt(ourInputValue[i]))
-        arrWithAttributes.push(ourInputValue[i + 1])
-        console.log('индекс числа',i)
+  let firstObj = {};
+  for (let i = 0; i < ourFirstInputValue.length; i += 1) {
+    if (parseInt(ourFirstInputValue[i])) {
+      arrWithAttributes.push(parseInt(ourFirstInputValue[i]));
+      arrWithAttributes.push(ourFirstInputValue[i + 1]);
+      firstObj[ourFirstInputValue[i + 1].slice(0, 4)] = ourFirstInputValue[i];
+      console.log(firstObj)
+      console.log(arrWithAttributes);
+      ourFirstObj = firstObj;
     }
+  }
 }
-  console.log(arrWithAttributes);
+
+
+
+
+//эта функция смотрит что сделал мальчик
+function inputActionValue() {
+  const ourActionInputValue = ourActionInput.value.split(' ');
+  let arrWithAttributes = [];
+  for (let i = 0; i < ourActionInputValue.length; i += 1) {
+    if (parseInt(ourActionInputValue[i])) {
+      if (actionsArray.includes(ourActionInputValue[i - 1])) {
+        arrWithAttributes.push(ourActionInputValue[i - 1]);
+        arrWithAttributes.push(parseInt(ourActionInputValue[i]));
+        arrWithAttributes.push(ourActionInputValue[i + 1]);
+        console.log(arrWithAttributes);
+      } else Notiflix.Notify.failure(`я не понимаю действие ${ourActionInputValue[i - 1]}`);
+    }
+  }
 }
+
+mainBtn.addEventListener('click', inputFirstValue)
