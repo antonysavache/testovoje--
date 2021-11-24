9232e0e798132dfafdc0f1b5b717854a619b2d3e
antonysavache <89783847+antonysavache@users.noreply.github.com>
2021-11-24 12:26:15 +0200

    неправильный перебор, сейчас буду копать

--- src/script.js
+++ src/script.js
@@ -2,74 +2,132 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const FRUITS_ARR = [
-  'абри',
-  'азим',
+  'абрик',
+  'азими',
+  'айвы',
   'айва',
-  'акеб',
+  'акеби',
   'аки',
-  'алыч',
-  'анан',
-  'анно',
-  'апел',
-  'араз',
-  'арха',
-  'астр',
-  'бакк',
-  'баку',
-  'бана',
+  'алыча',
+  'алычи',
+  'алыче',
+  'анана',
+  'аннон',
+  'апель',
+  'араза',
+  'архат',
+  'астро',
+  'бакко',
+  'бакуп',
+  'банан',
   'сапо',
-  'били',
-  'бояр',
-  'бром',
-  'бунх',
-  'вамп',
-  'вишн',
-  'воав',
-  'воск',
-  'ганд',
-  'гарц',
-  'гени',
-  'гете',
-  'гило',
+  'билим',
+  'бояры',
+  'брому',
+  'бунхо',
+  'вампи',
+  'вишня',
+  'вишни',
+  'воава',
+  'воско',
+  'ганда',
+  'гарци',
+  'генип',
+  'гетер',
+  'гилоц',
+  'слива',
   'слив',
-  'грев',
-  'грей',
-  'грум',
+  'сливы',
+  'греви',
+  'грейп',
+  'груми',
+  'груша',
+  'груши',
   'груш',
-  'гуав',
-  'гуай',
-  'дави',
-  'дакр',
-  'джам',
-  'джек',
-  'дови',
+  'гуава',
+  'гуавы',
+  'гуайя',
+  'давид',
+  'дакри',
+  'джамб',
+  'джекф',
+  'довиа',
   'дыня',
-  'жабо',
-  'земл',
-  'икак',
+  'дыни',
+  'дынь',
+  'жабот',
+  'земля',
+  'икако',
   'имбу',
+  'имбы',
+  'имб',
   'инга',
+  'инг',
+  'инги',
   'ирга',
-  'каим',
-  'кани',
-  'кант',
-  'капу',
-  'кара',
-  'кари',
-  'квин',
-  'кепе',
-  'кешь',
+  'ирги',
+  'ирг',
+  'каими',
+  'канис',
+  'канта',
+  'капул',
+  'карам',
+  'карис',
+  'квини',
+  'кепел',
+  'кешью',
   'киви',
-  'кизи',
-  'кокк',
-  'коко',
-  'ябло',
+  'кизил',
+  'кокко',
+  'кокон',
+  'кокос',
+  'корла',
+  'кумкв',
+  'купуа',
+  'лайм',
+  'лайма',
+  'лаймо',
+  'лангс',
+  'ларди',
+  'лимон',
+  'личи',
+  'лонга',
+  'лукум',
+  'лума',
+  'малин',
+  'манго',
+  'манда',
+  'марак',
+  'маран',
+  'некта',
+  'палай',
+  'папед',
+  'питай',
+  'питом',
+  'помел',
+  'ракум',
+  'рамбу',
+  'рангп',
+  'салак',
+  'сантол',
+  'сикан',
+  'страс',
+  'тамар',
+  'танже',
+  'фейхо',
+  'фикус',
+  'хурма',
+  'шелко',
+
+  'яблок',
 ];
 
 const ourFirstInput = document.querySelector('.js-first');
 const ourActionInput = document.querySelector('.js-action');
 const mainBtn = document.querySelector('.js-btn');
 const actionsArray = ['съел', 'положил', 'взял'];
+const divResult = document.querySelector('.js-inner-result')
+const peshka = document.createElement('p')
 
 // ourFirstInput.addEventListener('input', debounce(inputFirstValue, 300));
 // ourActionInput.addEventListener('input', debounce(inputActionValue, 300));
@@ -89,10 +147,8 @@ const actionsArray = ['съел', 'положил', 'взял'];
 
 // эта функция смотрит что было на столе и записывает в массив отфиль-
 //трованное значение
-
-// let ourFirstObj = null;
-
 function inputFirstValue() {
+  divResult.innerHTML = '';
   let actob = inputActionValue();
   const ourFirstInputValue = ourFirstInput.value.split(' ');
   let arrWithAttributes = [];
@@ -101,16 +157,15 @@ function inputFirstValue() {
   for (let i = 0; i < ourFirstInputValue.length; i += 1) {
     if (parseInt(ourFirstInputValue[i])) {
       arrWithAttributes.push(parseInt(ourFirstInputValue[i]));
-      arrWithAttributes.push(ourFirstInputValue[i + 1]);
+      arrWithAttributes.push(parseInt(ourFirstInputValue[i + 1]));
 
-      if (FRUITS_ARR.includes(ourFirstInputValue[i + 1].slice(0, 4)))
-      {
-        numOfFruits += parseInt(ourFirstInputValue[i])
+      if (FRUITS_ARR.includes(ourFirstInputValue[i + 1].slice(0, 5))) {
+        numOfFruits += parseInt(ourFirstInputValue[i]);
       }
       firstObj.fruits = numOfFruits;
-      firstObj[ourFirstInputValue[i + 1].slice(0, 4)] = ourFirstInputValue[i];
+      firstObj[ourFirstInputValue[i + 1].slice(0, 5)] = parseInt(ourFirstInputValue[i]);
       console.log('объект начального состояния', firstObj);
-      theActionWithStuf(firstObj, actob)
+      theActionWithStuf(firstObj, actob);
     }
   }
 }
@@ -127,7 +182,7 @@ function inputActionValue() {
         if (parseInt(ourActionInputValue[i])) {
           arrWithAttributes.push(parseInt(ourActionInputValue[i]));
           arrWithAttributes.push(ourActionInputValue[i + 1]);
-          actionObj[ourActionInputValue[i + 1].slice(0, 4)] = ourActionInputValue[i];
+          actionObj[ourActionInputValue[i + 1].slice(0, 5)] = parseInt(ourActionInputValue[i]);
           console.log('объект действий', actionObj);
           // console.log(arrWithAttributes);
           // ourFirstObj = firstObj;
@@ -135,24 +190,57 @@ function inputActionValue() {
       }
     }
   }
-  return actionObj
+  return actionObj;
 }
 
-function theActionWithStuf(firstObj, actionObj){
-  console.log(actionObj.action)
-  if (!actionsArray.includes(actionObj.action)){
-    console.log('Я не понял, что сделал мальчик')
-    return
-  } 
 
-const firstobj = Object.keys(firstObj);
-const secondobj = Object.keys(actionObj);
-firstobj.splice(0,1);
-secondobj.splice(0,1);
-console.log(firstobj,secondobj)
+
+function theActionWithStuf(firstObj, actionObj) {
+  console.log(actionObj.action);
+  if (!actionsArray.includes(actionObj.action)) {
+    divResult.innerHTML = `<p>я не понял, что сделал мальчик</p> `;
+    return;
+  }
+
+  if(actionObj.action === 'взял'){
+    take(firstObj, actionObj)
+  } else if (actionObj.action === 'положил'){
+    put(firstObj, actionObj)
+  }
+//парсим массивы для проверки все ли есть, что хочет взять мальчик
+
+}
+
+function take(firstObj, actionObj){
+  const firstobj = Object.keys(firstObj);
+  const secondobj = Object.keys(actionObj);
+  firstobj.splice(0, 1);
+  secondobj.splice(0, 1);
+  for (const iterator of secondobj) {
+    if(!firstobj.includes(iterator)){
+      return console.log(`${iterator} на столе не лежал`)
+    }
   
-  if (actionObj.action === 'взял') {
-      console.log('lox')
+    if(parseInt(firstObj[iterator]) > parseInt(actionObj[iterator]))
+    {
+      divResult.insertAdjacentHTML('beforeend',`<p>на столе осталось ${parseInt(firstObj[iterator]) - parseInt(actionObj[iterator])} от ${iterator}</p> `);
+      console.log(`на столе осталось ${parseInt(firstObj[iterator]) - parseInt(actionObj[iterator])} от ${iterator}`)
+    } else {
+      divResult.innerHTML = `<p>не хватает фрукта ${iterator}</p> `;
+      console.log(`не хватает ${iterator}`);
+    }
+    
+  }
+}
+
+function put(firstObj, actionObj){
+  const firstobj = Object.keys(firstObj);
+  const secondobj = Object.keys(actionObj);
+  firstobj.splice(0, 1);
+  secondobj.splice(0, 1);
+
+  for (const iterator of firstobj) {
+    firstObj[iterator] += parseInt(actionObj[iterator])
   }
 }
 
